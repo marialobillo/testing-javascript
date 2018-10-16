9c55b145978b4005979cf7627ab722236b30aff1
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-16 09:51:30 +0200

    adding lib, testing library

--- lesson-2/index.spec.js
+++ lesson-2/index.spec.js
@@ -1,7 +1,8 @@
-const { add } = require('index')
+const { add } = require('./index')
+const assert = require('assert')
 const mocha = require('mocha')
 
-describe('The add function'. () => {
+describe('The add function', () => {
 
   it('should return 11 for arg 5 and 6', () => {
     const result = add(5, 6)
@@ -11,7 +12,7 @@ describe('The add function'. () => {
   })
 
   it('should throw an error if arguments are not passed in', () => {
-    assert.throw(() => {
+    assert.throws(() => {
       add()
     })
   })
